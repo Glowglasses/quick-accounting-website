79ad540dffdde35ef0ebcb35f43eda88734ecdc9
miao <miao@163.com>
2021-08-30 03:36:36 +0800

    deploy

---
+++ precache-manifest.b589d472c9f1b78772efcba22788c214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723eb13a5283931492b9",
+    "url": "/css/app.272a7e1d.css"
+  },
+  {
+    "revision": "1c153292a3104d117c8f",
+    "url": "/css/chunk-vendors.56e44159.css"
+  },
+  {
+    "revision": "708be34e14d296b13c252baa06c4fcb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723eb13a5283931492b9",
+    "url": "/js/app.88f0bbe0.js"
+  },
+  {
+    "revision": "1c153292a3104d117c8f",
+    "url": "/js/chunk-vendors.0fc08a02.js"
+  },
+  {
+    "revision": "48845e7633c554359f3abf5d40ceb571",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
